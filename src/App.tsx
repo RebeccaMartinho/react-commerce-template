aedea6051964aff56504296fb88c499840dbbe5e
cherobim22@gmail.com <lucascherobim@hotmail.com>
2022-09-01 20:39:06 -0300

    app

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header';
+import CenterSearch from './components/Center';
+import Categories from './components/categories/Categories';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App p-4">
+      <div className="col-12">
+        <Header/>
+      </div>
+      <hr />
+      <div className="col-12 ">
+        <CenterSearch/>
+      </div>
+      <br />
+      <div className="col-12 ">
+        <Categories/>
+      </div>
     </div>
   );
 }
