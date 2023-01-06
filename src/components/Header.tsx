fd8118058448491242d9dde91b628738a819a82f
cherobim22@gmail.com <lucascherobim@hotmail.com>
2023-01-06 17:14:44 -0300

    header responsive

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -16,86 +16,49 @@ function Header() {
   const auth = useContext(AuthContext);
   const navigate = useNavigate();
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light rounded p-2">
-      <div className="container-fluid p-0">
-        <div className="d-flex w-25 align-items-center justify-content-around">
-          <img src={logo} width={60} alt="planeta-terra" />
-          <Dropdown>
-            <Dropdown.Toggle variant="success" id="dropdown-basic">
-              Categorias
-            </Dropdown.Toggle>
-
-            <Dropdown.Menu>
-              <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-              <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-              <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-            </Dropdown.Menu>
-          </Dropdown>
-        </div>
-
-        <div className="flex-shrink-1 d-flex w-50  ">
-          <div className="border rounded-2 bg-white input-group">
-            <div className="input-group-prepend d-flex align-items-center">
-              <span
-                className="bg-white border-0 shadow-none rounded-2 pe-0 input-group-text"
-                id="basic-addon1"
-              >
-                <Search />
-              </span>
-            </div>
-            <input
-              type="text"
-              className="border-0 shadow-none rounded-2 form-control"
-              placeholder="Products and reviews"
-              aria-label="Products and reviews"
-              aria-describedby="basic-addon1"
-            />
-          </div>
-        </div>
-
-        <div
-          className="collapse navbar-collapse navbar-expanded"
-          id="navbarCollapse"
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">
+          Navbar
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarTogglerDemo01"
+          aria-controls="navbarTogglerDemo01"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
         >
-          <div className="navbar-nav ms-auto">
-            <div className="d-flex align-items-center lh-lg gap-5 fw-bold ms-3">
-              <a href="/notifications" className="">
-                <Bell color="black" size={25} />
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="#">
+                Home
               </a>
-              <span
-                // on:click="{openFormModal}"
-                className="cursor-pointer text-decoration-none text-gray-700"
-              >
-                <CartCheck size={25} />
-              </span>
-              {auth.user ? (
-                <span
-                  onClick={() => {
-                    navigate("/dashboard");
-                  }}
-                  className="cursor-pointer text-decoration-none text-gray-700"
-                >
-                  <Person size={25} />
-                </span>
-              ) : (
-                <span
-                  onClick={() => {
-                    navigate("/login");
-                  }}
-                  className="cursor-pointer text-decoration-none text-gray-700"
-                >
-                  <ArrowUpRightCircle size={25} />
-                </span>
-              )}
-              <div
-                // on:click="{logout}"
-                style={{ width: "100px" }}
-                className="cursor-pointer text-decoration-none text-gray-700 d-flex justify-content-around align-items-center"
-              >
-                <Clock color="green" size={25} /> <small>Open</small>
-              </div>
-            </div>
-          </div>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Link
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link disabled">Disabled</a>
+            </li>
+          </ul>
+          <form className="d-flex" role="search">
+            <input
+              className="form-control me-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            />
+            <button className="btn btn-outline-success" type="submit">
+              Search
+            </button>
+          </form>
         </div>
       </div>
     </nav>
