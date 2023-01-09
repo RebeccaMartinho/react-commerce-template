b455e11013a3fbe27bfb7060d8a1b6fa63f40772
cherobim22@gmail.com <lucascherobim@hotmail.com>
2023-01-08 21:59:34 -0300

    fix responsiveness

--- src/components/categories/Categories.tsx
+++ src/components/categories/Categories.tsx
@@ -1,26 +1,28 @@
-import { Search } from "react-bootstrap-icons"
-import oPlanet from '../../public/proteja-o-planeta.png'
-import Item from  './Item'
+import { Search } from "react-bootstrap-icons";
+import oPlanet from "../../public/proteja-o-planeta.png";
+import Item from "./Item";
 
-function Categories(){
-    return(
-       <div className="col-12 d-flex flex-column justify-content-center align-items-center">
-        <div className="col-10 d-flex justify-content-between p-2 align-items-center">
-            <h3>Categories</h3>
-            <a href="/" style={{textDecoration:"none", color:"red"}}>Show All</a>
-        </div>
-        <br />
-        <div className="col-10 d-flex justify-content-between align-items-center">
-        <Item image="tela.png"/>
-            <Item image="chamada-telefonica.png"/>
-            <Item image="computador-portatil.png"/>
-            <Item image="video.png"/>
-            <Item image="pen-drive-usb.png"/>
-            <Item image="sazonal.png"/>
-            <Item image="floresta.png"/>
-        </div>
-       </div>
-    )
+function Categories() {
+  return (
+    <div className="col-12 d-flex flex-column justify-content-center align-items-center">
+      <div className="col-10 d-flex justify-content-between p-2 align-items-center">
+        <h3>Categories</h3>
+        <a href="/" style={{ textDecoration: "none", color: "red" }}>
+          Show All
+        </a>
+      </div>
+      <br />
+      <div className="col-10 d-flex flex-wrap justify-content-center justify-content-md-between  align-items-center">
+        <Item image="tela.png" />
+        <Item image="chamada-telefonica.png" />
+        <Item image="computador-portatil.png" />
+        <Item image="video.png" />
+        <Item image="pen-drive-usb.png" />
+        <Item image="sazonal.png" />
+        <Item image="floresta.png" />
+      </div>
+    </div>
+  );
 }
 
-export default Categories
+export default Categories;
--- src/components/features/Features.tsx
+++ src/components/features/Features.tsx
@@ -1,28 +1,28 @@
-import { Search } from "react-bootstrap-icons"
-import oPlanet from '../../public/proteja-o-planeta.png'
-import Item from "./FeatureItem"
+import { Search } from "react-bootstrap-icons";
+import oPlanet from "../../public/proteja-o-planeta.png";
+import Item from "./FeatureItem";
 
-
-function Features(){
-    return(
-       <div className="col-12 d-flex flex-column justify-content-center align-items-center">
-        <div className="col-10 d-flex justify-content-between p-2 align-items-center">
-            <h3>Feature Topics</h3>
-            <a href="/" style={{textDecoration:"none", color:"red"}}>Show All</a>
-        </div>
-        <br />
-        <div className="col-10 d-flex justify-content-between align-items-center">
-            <Item size="280px" image="features/amazon.jpeg"/>
-            <Item size="280px" image="features/amazon.jpeg"/>
-            <Item size="280px" image="features/amazon.jpeg"/>
-            <Item size="280px" image="features/amazon.jpeg"/>
-            {/* <Item image="chamada-telefonica.png"/>
+function Features() {
+  return (
+    <div className="col-12 d-flex flex-column justify-content-center align-items-center">
+      <div className="col-10 d-flex justify-content-between p-2 align-items-center">
+        <h3>Feature Topics</h3>
+        <a href="/" style={{ textDecoration: "none", color: "red" }}>
+          Show All
+        </a>
+      </div>
+      <br />
+      <div className="col-10 d-flex flex-wrap justify-content-between align-items-center">
+        <Item size="280px" image="features/amazon.jpeg" />
+        <Item size="280px" image="features/amazon.jpeg" />
+        <Item size="280px" image="features/amazon.jpeg" />
+        <Item size="280px" image="features/amazon.jpeg" />
+        {/* <Item image="chamada-telefonica.png"/>
             <Item image="computador-portatil.png"/>
             <Item image="video.png"/> */}
-       
-        </div>
-       </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-export default Features
+export default Features;
