f89fe81c64792e6bdd0d2f9f884bdcf050818a1a
cherobim22 <lucascherobim@hotmail.com>
2023-04-30 19:46:22 -0300

    adjust product card

--- src/components/features/Product.tsx
+++ src/components/features/Product.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 
+import image from "../../public/rollingreen/popi.jpeg";
 interface ImageText {
   image: string;
   size: string;
@@ -8,65 +9,30 @@ interface ImageText {
 function Product(props: ImageText) {
   return (
     <div
-      className="container d-flex justify-content-center"
-      style={{ width: "30%" }}
+      className="card d-flex flex-column align-items-center justify-content-center "
+      style={{ width: "22%" }}
     >
-      <figure className="card card-product-grid card-lg">
-        {" "}
-        <a href="#" className="img-wrap" data-abc="true">
-          {" "}
-          <img src="https://i.imgur.com/MPqUt62.jpg" />{" "}
-        </a>
-        <figcaption className="info-wrap">
-          <div className="row">
-            <div className="col-md-9 col-xs-9">
-              {" "}
-              <a href="#" className="title" data-abc="true">
-                Dell Xtreme 270
-              </a>{" "}
-              <span className="rated">Laptops</span>{" "}
-            </div>
-            <div className="col-md-3 col-xs-3">
-              <div className="rating text-right">
-                {" "}
-                <i className="fa fa-star"></i> <i className="fa fa-star"></i>{" "}
-                <i className="fa fa-star"></i> <i className="fa fa-star"></i>{" "}
-                <span className="rated">Rated 4.0/5</span>{" "}
-              </div>
-            </div>
-          </div>
-        </figcaption>
-        <div className="bottom-wrap-payment">
-          <figcaption className="info-wrap">
-            <div className="row">
-              <div className="col-md-9 col-xs-9">
-                {" "}
-                <a href="#" className="title" data-abc="true">
-                  $3,999
-                </a>{" "}
-                <span className="rated">VISA Platinum</span>{" "}
-              </div>
-              <div className="col-md-3 col-xs-3">
-                <div className="rating text-right"> #### 8787 </div>
-              </div>
-            </div>
-          </figcaption>
-        </div>
-        <div className="bottom-wrap">
-          {" "}
-          <a href="#" className="btn btn-primary float-right" data-abc="true">
-            {" "}
-            Buy now{" "}
-          </a>
-          <div className="price-wrap">
-            {" "}
-            <a href="#" className="btn btn-warning float-left" data-abc="true">
-              {" "}
-              Cancel{" "}
-            </a>{" "}
-          </div>
-        </div>
-      </figure>
+      <img className="" width={"80%"} src={image} />
+      <div className="w-100 d-flex flex-column align-items-start justify-content-center">
+        <span className="text-start p-1" data-abc="true">
+          BANDEJA DE METAL G ROLLZ GRANDE DOG EINSTEIN
+        </span>
+        <span className="text-start p-1">Bandejas</span>
+      </div>
+
+      <div className="w-100 d-flex flex-column align-items-start justify-content-center">
+        <hr className="w-100" />
+        <span className="text-start p-1" data-abc="true">
+          R$ 3,999
+        </span>
+        <span className="text-start p-1 ">Pix, Credito, Boleto Parcelado</span>
+        <hr className="w-100" />
+      </div>
+      <div className="w-100 d-flex align-items-center justify-content-center mb-3">
+        <button className="btn btn-primary float-right" data-abc="true">
+          Adicionar ao carrinho
+        </button>
+      </div>
     </div>
   );
 }
